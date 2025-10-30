46598e6d4e8f51cca66444286cee9b528ab9b1c3
IBBRA533 <ibraputra38@gmail.com>
2025-10-30 13:20:27 +0700

    soal satu buat navbar

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="id">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>UTS Laravel</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar bg-light border-bottom">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-semibold" href="#">UTS Laravel</a>
+<h2>Halaman Home</h2>
+    <button class="btn btn-outline-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      ☰
+    </button>
+  </div>
+</nav>
+</body>
+</html>
